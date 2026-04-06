8466dcdb49847f1f1b272d3e42daa0bdfb779adc
syunnjack <syunnjack@gmail.com>
2026-04-06 21:12:34 +0900

    fix: remove incorrect css import

--- app/area/[id]/page.tsx
+++ app/area/[id]/page.tsx
@@ -1,6 +1,6 @@
 // app/layout.tsx
 import type { Metadata } from "next";
-import "./globals.css";
+//import "./globals.css";
 
 export const metadata: Metadata = {
     title: "全国市区町村 蓄電池・V2H補助金ナビ",
