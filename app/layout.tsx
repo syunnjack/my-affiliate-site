0d001de198bd6ae97d350025aa9c0ccaf563e1aa
syunnjack <syunnda1@yahoo.co.jp>
2026-08-04 15:05:22 +0900

    Add GA4 tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ export default function RootLayout({
     children: React.ReactNode;
 }) {
     return (
-        <html lang="ja">
+        <html lang="ja"><head><script async src="https://www.googletagmanager.com/gtag/js?id=G-KFW7PX4BSQ"></script><script dangerouslySetInnerHTML={{__html: `window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-KFW7PX4BSQ');`}} /></head>
             <body>
                 <body
                     style={{
